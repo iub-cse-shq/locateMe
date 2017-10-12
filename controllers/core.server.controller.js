2ddb7cdcd67cf0937c78774fd43644b5be46b697
Shama Hoque <>
2017-10-12 03:25:21 +0000

    INTRO

--- controllers/core.server.controller.js
+++ controllers/core.server.controller.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 exports.index = function(req, res) {
-	res.render('./../index.ejs', {
+	res.render('./../public/views/index2.ejs', {
 		user: req.user || null,
 		request: req
 	});
